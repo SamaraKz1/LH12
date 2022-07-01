c0fac795006be545a53e40e4783b8ba3671de390
SamaraKz1 <samara.keshavarz@gmail.com>
2022-07-01 17:01:37 +0200

    commit share_ui

--- share_ui.py
+++ share_ui.py
@@ -2,14 +2,13 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-#from gsheetsdb import connect
+from gsheetsdb import connect
 
 st.title("My First Web App")
 
 #st.title("Connect to Google Sheets")
 gsheet_url_data = st.secrets["gsheet_url_data"]
-#conn = connect()
-#rows = conn.execute(f'SELECT * FROM "{gsheet_url_data}"')
-#df_gsheet = pd.DataFrame(rows)
-#st.write(df_gsheet)
-st.write(gsheet_url_data)
+conn = connect()
+rows = conn.execute(f'SELECT * FROM "{gsheet_url_data}"')
+df_gsheet = pd.DataFrame(rows)
+st.write(df_gsheet)
