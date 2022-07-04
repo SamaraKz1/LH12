f1981bc439b3ce2b5b20218152c91f679e70261e
SamaraKz1 <samara.keshavarz@gmail.com>
2022-07-04 11:38:13 +0200

    commit share_ui

--- share_ui.py
+++ share_ui.py
@@ -75,6 +75,11 @@ span[data-baseweb="tag"] {
     unsafe_allow_html=True,
 )
 options = st.multiselect("Select mutiple descriptions to compare", data['DESCRIPTION'].unique())
+vectorizer = CountVectorizer(input='content', max_features=200)
+wordcounts = vectorizer.fit_transform(descriptions).toarray()
+cosine_dist = pd.DataFrame(squareform(pdist(wordcounts, metric='cosine')), index=options, columns=options)
+st.wrtie(cosine_dist)
+
 #------------------ Find similarities -----------------
 
 
