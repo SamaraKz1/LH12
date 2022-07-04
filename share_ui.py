870d037f9e7896851d26c23594819489446296fa
SamaraKz1 <samara.keshavarz@gmail.com>
2022-07-04 09:07:17 +0200

    commit share_ui

--- share_ui.py
+++ share_ui.py
@@ -32,61 +32,7 @@ st.write('yes')
 
 #data = = pd.DataFrame(rows)
 
-descriptions = sorted(set(data['DESCRIPTION']))
-vectorizer = CountVectorizer(input='content', max_features=2500)
-wordcounts = vectorizer.fit_transform(descriptions).toarray()
 
-#----------------------TITLES------------------
-st.set_page_config(page_title="LH12 Price Recommendation", layout="wide", page_icon='📝') 
-st.title("Recommendation for substitute product")
-
-#--------------------- SIDEBAR ----------------
-
-st.sidebar.title("Product Description")
-category = st.sidebar.selectbox("Select category group area", ['Site Products & Logistics', 'IT (Server & Storage)'])
-
-description = st.sidebar.selectbox("Select description", data['DESCRIPTION_TEXT'].unique())
-
-list_specifications = data[data['DESCRIPTION_TEXT']==description]['DESCRIPTION'].unique()
-product = st.sidebar.selectbox("Select specifications", list_specifications)
-
-st.sidebar.title("Product Comparison")
-st.markdown(
-    """
-<style>
-span[data-baseweb="tag"] {
-  background-color: grey !important;
-}
-</style>
-""",
-    unsafe_allow_html=True,
-)
-options = st.sidebar.multiselect("Select mutiple descriptions to compare", data['DESCRIPTION'].unique())
-
-#---------------------- Body ------------------
-
-st.write(" The product info for the selected specification:")
-
-#if category == 'Site Products & Logistics':
-
-st.dataframe(data[(data['DESCRIPTION']==product)].reset_index(drop=True).style.format({"LOCAL_PRICE": "{:.2f}"}))
-
-st.write(""" ## 📊 Substitude products: """)
-#distances.index = distances.columns
-
-n_neigh = st.selectbox("Number of substitude products to recommend", [i+1 for i in range(20)])
-
-idx = descriptions.index(product)
-
-
-distances = cdist(wordcounts[wordcounts].reshape(1,-1), wordcounts, metric='cosine')
-distances = pd.DataFrame(distances, index=descriptions, columns = ['Distance']).reset_index().rename(columns = {'index':'PRODNO'})
-neighbors = distances.nsmallest(n_neigh+1, 'Distance')
-
-
-neigh_prod = pd.merge(neighbors, data, on=['DESCRIPTION'], how='in').sort_values('Distance').reset_index(drop=True)
-
-st.write(neigh_prod)
 
 #for i in range(len(neighbors)):
 #    neigh_prod.loc[neigh_prod['DESCRIPTION'] == neighbors.index[i], 'Distance'] = neighbors.values[i]
