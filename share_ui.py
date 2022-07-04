ad506ce165ebc2bcf80b2f6bbb55ecb134ef29d5
SamaraKz1 <samara.keshavarz@gmail.com>
2022-07-04 10:56:21 +0200

    commit share_ui

--- share_ui.py
+++ share_ui.py
@@ -68,12 +68,10 @@ n_neigh = st.selectbox("Number of substitude products to recommend", [i+1 for i 
 
 idx = descriptions.index(product)
 
-st.write(idx, wordcounts.shape, wordcounts[idx].reshape(1,-1).shape)
-
-#distances = cdist(wordcounts, wordcounts[idx].reshape(1,-1), metric='cosine')
-#distances = pd.DataFrame(distances, index=descriptions, columns = ['Distance']).reset_index().rename(columns = {'index':'PRODNO'})
-#neighbors = distances.nsmallest(n_neigh+1, 'Distance')
-#st.write(neighbors)
+distances = cdist(wordcounts, wordcounts[idx].reshape(1,-1), metric='cosine')
+distances = pd.DataFrame(distances, index=descriptions, columns = ['Distance']).reset_index().rename(columns = {'index':'PRODNO'})
+neighbors = distances.nsmallest(n_neigh+1, 'Distance')
+st.write(neighbors)
 
 #neigh_prod = pd.merge(neighbors, data, on=['DESCRIPTION'], how='in').sort_values('Distance').reset_index(drop=True)
 
