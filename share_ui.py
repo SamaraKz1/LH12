d8539b8b8f064b39a74d54d952ba280f29a9b881
SamaraKz1 <samara.keshavarz@gmail.com>
2022-07-07 19:05:20 +0200

    commit share_ui

--- share_ui.py
+++ share_ui.py
@@ -136,7 +136,7 @@ def get_comparison(df, prod_desc, prod_col, desc_col):
         options = st.multiselect("Select mutiple descriptions to compare", df[desc_col].unique())
     
     if options:
-        st.write(df[df[desc_col].drop(['DESCRIPTION_stem','DESCRIPTION_TEXT'], axis=1).isin(options)])
+        st.write(df[df[desc_col].isin(options)].drop(['DESCRIPTION_stem','DESCRIPTION_TEXT'], axis=1))
 
     return options
 
