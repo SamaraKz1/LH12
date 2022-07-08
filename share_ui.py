0e5cbefe9b7fc70bcf86efcf799e5d0d65fd5b0f
SamaraKz1 <samara.keshavarz@gmail.com>
2022-07-08 15:42:06 +0200

    commit share_ui

--- share_ui.py
+++ share_ui.py
@@ -153,7 +153,7 @@ def calculate_distance(options):
     #st.write(cosine_dist)
     col1, col2 = st.columns([3, 1])
     fig, ax = plt.subplots()
-    sns.heatmap(cosine_dist, vmin = 0, vmax = 1, cmap = 'coolwarm', ax=ax, linewidths=2, annot=True, fmt='.2g')
+    sns.heatmap(cosine_dist, vmin = 0, vmax = 1, cmap = 'coolwarm', ax=ax, linewidths=2, annot=True, fmt='.2f')
     col1.pyplot(fig)
 
 
