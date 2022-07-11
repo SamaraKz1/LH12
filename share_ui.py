26891bc8319c149df9e09cff8845666ac2517082
SamaraKz1 <samara.keshavarz@gmail.com>
2022-07-11 17:07:00 +0200

    modify share_ui

--- share_ui.py
+++ share_ui.py
@@ -14,7 +14,7 @@ from scipy.spatial.distance import pdist, cdist, squareform
 
 from gsheetsdb import connect
 
-st.set_page_config(page_title="LH12 Price Recommendation", layout="wide", page_icon='📝') 
+st.set_page_config(page_title="LH12 Price Recommendation", layout="wide", page_icon="📝")
 
 
 @st.cache(allow_output_mutation=True, hash_funcs={pd.DataFrame: lambda _: None})
@@ -24,55 +24,77 @@ def get_df(gsheet_url):
     df = pd.DataFrame(rows)
     return df
 
+
 @st.cache(allow_output_mutation=True)
 def word2vec(descs):
     list_desc = sorted(set(descs))
-    vectorizer = CountVectorizer(input='content', max_features=2000)
+    vectorizer = CountVectorizer(input="content", max_features=2000)
     wordcounts = vectorizer.fit_transform(list_desc).toarray()
 
     return wordcounts
 
 
 def show_sidebar(df, prod_col, desc_col):
-    if prod_desc == 'Product Number':
-        prodno = st.sidebar.selectbox("Select product number", sorted(df[prod_col].unique()))
-        prod_list = list(df[df[prod_col]==prodno][desc_col])
+    if prod_desc == "Product Number":
+        prodno = st.sidebar.selectbox(
+            "Select product number", sorted(df[prod_col].unique())
+        )
+        prod_list = list(df[df[prod_col] == prodno][desc_col])
         product = max(prod_list, key=len)
-        
-    elif prod_desc == 'Description':
-        product = st.sidebar.selectbox("Select description", sorted(df[desc_col].unique()))
+
+    elif prod_desc == "Description":
+        product = st.sidebar.selectbox(
+            "Select description", sorted(df[desc_col].unique())
+        )
 
     return product
 
+
 data_swb = get_df(st.secrets["gsheet_url_swb"])
 data_po = get_df(st.secrets["gsheet_url_po"])
-swb_words = word2vec(data_swb['DESCRIPTION'])
-po_words = word2vec(data_po['MaterialDesc'])
+swb_words = word2vec(data_swb["DESCRIPTION"])
+po_words = word2vec(data_po["MaterialDesc"])
 
 
-#==========================TITLES=======================
+# ==========================TITLES=======================
 st.title("Recommendation for substitute product")
 st.write(" The product info for the selected specification:")
-#==========================SIDEBAR & Body===============
+# ==========================SIDEBAR & Body===============
 
 st.sidebar.title("Product Description")
-category = st.sidebar.selectbox("Select category group area", ['Site Products & Logistics', 'IT (Server & Storage)'])
+category = st.sidebar.selectbox(
+    "Select category group area", ["Site Products & Logistics", "IT (Server & Storage)"]
+)
 
-prod_desc = st.sidebar.selectbox("Select filter", ['Product Number', 'Description'])
+prod_desc = st.sidebar.selectbox("Select filter", ["Product Number", "Description"])
 
-if category == 'Site Products & Logistics':
-    swb_product = show_sidebar(data_swb, 'PRODNO', 'DESCRIPTION')
+if category == "Site Products & Logistics":
+
+    commodity_list = ['All'] + sorted(data_swb["COMMODITY_AREA_NAME"].unique())
+    swb_commodity = st.sidebar.selectbox("Select commodity area", commodity_list)
+
+    if swb_commodity == 'All':
+        data_swb_commodity = data_swb.copy()
+    else:
+        data_swb_commodity = data_swb[data_swb["COMMODITY_AREA_NAME"]==swb_commodity]
+
+    swb_product = show_sidebar(data_swb_commodity, "PRODNO", "DESCRIPTION")
+
     st.dataframe(
-        data_swb[(data_swb['DESCRIPTION']==swb_product)].reset_index(drop=True).style.format({"LOCAL_PRICE": "{:.2f}"})
-        )
-    
-elif category == 'IT (Server & Storage)':
-    po_product = show_sidebar(data_po, 'MaterialWithoutRState', 'MaterialDesc')
-    st.dataframe(data_po[(data_po['MaterialDesc']==po_product)].reset_index(drop=True))
+        data_swb_commodity[(data_swb_commodity["DESCRIPTION"] == swb_product)]
+        .reset_index(drop=True)
+        .style.format({"LOCAL_PRICE": "{:.2f}"})
+    )
 
 
+elif category == "IT (Server & Storage)":
+    po_product = show_sidebar(data_po, "MaterialWithoutRState", "MaterialDesc")
+    st.dataframe(
+        data_po[(data_po["MaterialDesc"] == po_product)].reset_index(drop=True)
+    )
 
-#============================Find Substitude Products==============================
+
+# ============================Find Substitude Products==============================
 
 
 st.write(""" ## 📊 Substitute products: """)
@@ -84,38 +106,50 @@ st.write(
     Note: Distnace is a metric that represents how different the substitude products are to the one you selected. \\
     It varies from 0 (most similar) to 1 (least similar).
     """
-    )
+)
+
 
 def find_neighbors(df, product, wordcounts):
     descriptions = sorted(set(df))
     idx = descriptions.index(product)
 
-    distances = cdist(wordcounts, wordcounts[idx].reshape(1,-1), metric='cosine')
-    distances = pd.DataFrame(
-        distances, index=descriptions, columns = ['Distance']
-        ).reset_index().rename(columns = {'index':df.name})
-    neighbors = distances.nsmallest(n_neigh+1, 'Distance')
+    distances = cdist(wordcounts, wordcounts[idx].reshape(1, -1), metric="cosine")
+    distances = (
+        pd.DataFrame(distances, index=descriptions, columns=["Distance"])
+        .reset_index()
+        .rename(columns={"index": df.name})
+    )
+    neighbors = distances.nsmallest(10 * n_neigh, "Distance")
 
     return neighbors
 
 
 def merge_dfs(df1, df2, key):
-    neigh_prod = pd.merge(df1, df2, on=key, how='inner').sort_values('Distance').reset_index(drop=True)
+    neigh_prod = (
+        pd.merge(df1, df2, on=key, how="inner")
+        .sort_values("Distance")
+        .reset_index(drop=True)
+    )
     return neigh_prod
 
-if category == 'Site Products & Logistics':
-    df_neighbors = find_neighbors(data_swb['DESCRIPTION'], swb_product, swb_words)
-    neigh_prod = merge_dfs(df_neighbors, data_swb, 'DESCRIPTION')
 
-    st.dataframe(neigh_prod.style.format({"LOCAL_PRICE": "{:.2f}", "Distance": "{:.3f}"}))
+if category == "Site Products & Logistics":
+    df_neighbors = find_neighbors(data_swb["DESCRIPTION"], swb_product, swb_words)
+    neigh_prod = merge_dfs(df_neighbors, data_swb_commodity, "DESCRIPTION")
+    neigh_prod = neigh_prod.nsmallest(n_neigh+1, "Distance").reset_index(drop=True)
 
-elif category == 'IT (Server & Storage)':
-    df_neighbors = find_neighbors(data_po['MaterialDesc'], po_product, po_words)
-    neigh_prod = merge_dfs(df_neighbors, data_po, 'MaterialDesc')
+    st.dataframe(
+        neigh_prod.style.format({"LOCAL_PRICE": "{:.2f}", "Distance": "{:.3f}"})
+    )
+
+
+elif category == "IT (Server & Storage)":
+    df_neighbors = find_neighbors(data_po["MaterialDesc"], po_product, po_words)
+    neigh_prod = merge_dfs(df_neighbors, data_po, "MaterialDesc")
 
     st.dataframe(neigh_prod.style.format({"Distance": "{:.3f}"}))
 
-#=========================Find similarities===========================
+# =========================Find similarities===========================
 st.write(""" ## 📊 Compare given products: """)
 
 st.markdown(
@@ -131,13 +165,17 @@ span[data-baseweb="tag"] {
 
 
 def get_comparison(df, prod_desc, prod_col, desc_col):
-    if prod_desc == 'Product Number':
-        products = st.multiselect("Select mutiple products to compare", sorted(df[prod_col].unique()))
+    if prod_desc == "Product Number":
+        products = st.multiselect(
+            "Select mutiple products to compare", sorted(df[prod_col].unique())
+        )
         options = list(set(df[df[prod_col].isin(products)][desc_col]))
 
-    elif prod_desc == 'Description':
-        options = st.multiselect("Select mutiple descriptions to compare", df[desc_col].unique())
-    
+    elif prod_desc == "Description":
+        options = st.multiselect(
+            "Select mutiple descriptions to compare", df[desc_col].unique()
+        )
+
     if options:
         st.write(df[df[desc_col].isin(options)])
 
@@ -145,29 +183,36 @@ def get_comparison(df, prod_desc, prod_col, desc_col):
 
 
 def calculate_distance(options):
-    vectorizer = CountVectorizer(input='content', max_features=1000)
+    vectorizer = CountVectorizer(input="content", max_features=1000)
     wordcounts = vectorizer.fit_transform(options).toarray()
-    cosine_dist = pd.DataFrame(squareform(pdist(wordcounts, metric='cosine')), index=options, columns=options)
+    cosine_dist = pd.DataFrame(
+        squareform(pdist(wordcounts, metric="cosine")), index=options, columns=options
+    )
     cosine_dist = np.round(cosine_dist * 100)
-    #st.write(cosine_dist)
+    # st.write(cosine_dist)
     col1, col2 = st.columns([3, 1])
     fig, ax = plt.subplots()
-    sns.heatmap(cosine_dist, vmin = 0, vmax = 100, cmap = 'coolwarm', ax=ax, linewidths=2, annot=True, fmt="g")
+    sns.heatmap(
+        cosine_dist,
+        vmin=0,
+        vmax=100,
+        cmap="coolwarm",
+        ax=ax,
+        linewidths=2,
+        annot=True,
+        fmt="g",
+    )
     col1.pyplot(fig)
 
 
-if category == 'Site Products & Logistics':
-    options = get_comparison(data_swb, prod_desc, 'PRODNO', 'DESCRIPTION')
+if category == "Site Products & Logistics":
+    options = get_comparison(data_swb_commodity, prod_desc, "PRODNO", "DESCRIPTION")
 
-elif category == 'IT (Server & Storage)':
-    options = get_comparison(data_po, prod_desc, 'MaterialWithoutRState', 'MaterialDesc')    
+elif category == "IT (Server & Storage)":
+    options = get_comparison(
+        data_po, prod_desc, "MaterialWithoutRState", "MaterialDesc"
+    )
 
 
 if options:
     calculate_distance(options)
-
-
-
-
-
-
